238134ec12010d81a0c437fb1f56b6bd1df774e2
Varsha4321 <79386531+Varsha4321@users.noreply.github.com>
2021-08-20 17:46:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized")
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+    }
+}
+function draw(){
+    background('#969A97');
+    document.getElementById("text_size").innerHTML = "width and height of this text will be = " + difference + "px";
+    fill('#F90093');
+    square(noseX, noseY, difference);
+}
